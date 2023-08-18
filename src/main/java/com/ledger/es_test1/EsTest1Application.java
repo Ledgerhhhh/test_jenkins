e4e14a3ba00e5bfe66f1e9210b1a0c66c28b0ba5
1·231 <123213>
2023-08-18 14:20:09 +0800

    11

---
+++ src/main/java/com/ledger/es_test1/EsTest1Application.java
@@ -0,0 +1,13 @@
+package com.ledger.es_test1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsTest1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsTest1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ledger/es_test1/EsTest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ledger.es_test1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsTest1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
